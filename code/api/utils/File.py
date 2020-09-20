0c04dc9066b897c854150d12e7cdf4f124fc285b
benwoo1110 <benedict_woo_jun_kai@s2016.sst.edu.sg>
2020-09-20 23:41:40 +0800

    refractored file object

--- code/api/utils/File.py
+++ code/api/utils/File.py
@@ -9,6 +9,9 @@ from code.api.utils.Logger import Logger
 class File:
     def __init__(self, filepath):
         self.filepath = os.path.join(filepath)
+    
+    def __repr__(self):
+        return "File('{}')".format(self.getPath())
 
     def createDirIfEmpty(self) -> bool:
         if os.path.isdir(self.filepath):
@@ -21,30 +24,12 @@ class File:
         return True
 
     def loadYaml(self) -> dict:
-        # Read from config file
-        with open(self.filepath, "r") as yamlFile:
-            return yaml.load(yamlFile, Loader=yaml.FullLoader)
-
-    def exist(self) -> bool:
-        return os.path.exists(self.filepath)
-
-    def getContainingFiles(self, filter_:str = "*", withExtension:bool = True, fileObject:bool = True) -> dict:
-        files = dict()
-        for file_ in glob.glob(os.path.join(self.filepath, filter_)):
-            if not os.path.isfile(file_):
-                continue
-            files[File.getFileName(file_, withExtension)] = File(file_) if fileObject else file_
-        return files
-
-    def getName(self, withExtension:bool = True):
-        return File.getFileName(self.filepath, withExtension)
-
-    @staticmethod
-    def getFileName(filepath:str, withExtension:bool = True):
-        if not os.path.isfile(filepath):
-            raise AttributeError("Path '{}' is not a file!".format(filepath))
-        fileName = os.path.basename(filepath)
-        return fileName if withExtension else fileName.split(".")[0]
+        try:
+            with open(self.filepath, "r") as yamlFile:
+                return yaml.load(yamlFile, Loader=yaml.FullLoader)
+        except Exception as e:
+            Logger.get().error(e, exc_info=True)
+            return None
 
     def copyTo(self, dst) -> bool:
         try:
@@ -52,8 +37,26 @@ class File:
         except Exception as e:
             Logger.get().error("Error copying file", exc_info=True)
 
+    def exist(self) -> bool:
+        return os.path.exists(self.filepath)
+
+    def getContainingFiles(self, filter_:str = "*", withExtension:bool = True, fileObject:bool = True, nested:bool = False) -> dict:
+        files = dict()
+        for file_ in glob.glob(os.path.join(self.filepath, filter_), recursive=nested):
+            if not os.path.isfile(file_):
+                continue
+            files[File.getFileName(file_, withExtension)] = File(file_) if fileObject else file_
+        return files
+
+    def getName(self, withExtension:bool = True):
+        return File.getFileName(self.filepath, withExtension)
+    
     def getPath(self) -> str:
         return self.filepath
 
-    def __repr__(self):
-        return "File('{}')".format(self.getPath())
+    @staticmethod
+    def getFileName(filepath:str, withExtension:bool = True):
+        if not os.path.isfile(filepath):
+            raise AttributeError("Path '{}' is not a file!".format(filepath))
+        fileName = os.path.basename(filepath)
+        return fileName if withExtension else fileName.split(".")[0]
