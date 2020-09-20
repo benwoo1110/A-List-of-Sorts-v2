087167154028afe50f6fc5be1041a5db82f6e3da
benwoo1110 <benedict_woo_jun_kai@s2016.sst.edu.sg>
2020-09-20 23:05:05 +0800

    Added surface data for home screen

---
+++ code/api/event/Events.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class Events:
+
+    def click(self, event):
+        pass
+
+    def keydown(self, event):
+        pass
+        
+    def keyup(self, event):
+        pass
+
+    def quit(self, event):
+        pass
--- code/screens/home.py
+++ code/screens/home.py
@@ -2,7 +2,7 @@ from code.api.core.Screen import Screen
 from code.api.core.Surface import Surface
 from code.api.core.Frame import Frame
 from code.api.data.Images import Images
-from code.api.data.Text import Text
+from code.api.data.Text import Text, TextFormat
 
 
 class home(Screen):
@@ -12,23 +12,69 @@ class home(Screen):
         self\
         .addSurface(
             Surface(
+                name="type",
+                type_="button",
+                frame=Frame(x=246, y=149, w=729, h=70),
+                actions=[]
+            )
+            .addData("image", Images(None, Frame(x=246, y=149, w=729, h=70)))
+            .addData("text", Text(
+                frame=Frame(x=271, y=159, w=628, h=53),
+                text="Insertion sort",
+                format=TextFormat(fontSize=38, pos="center")
+            ))
+        )\
+        .addSurface(
+            Surface(
                 name="info",
                 type_="button",
                 frame=Frame(x=225, y=242, w=751, h=211),
                 actions=[]
             )
             .addData("image", Images(None, Frame(x=225, y=242, w=751, h=211)))
-            .addChild(
-                Surface(
-                    name="test",
-                    type_="button",
-                    frame=Frame(x=225, y=242, w=751, h=211),
-                    actions=[]
-                )
-                .addData("image", Images(None, Frame(x=225, y=242, w=751, h=211)))
-                .addData("text", Text(frame=Frame(x=100, y=242, w=751, h=211), text="test"))
+            .addData("text", Text(
+                frame=Frame(x=252, y=260, w=695, h=168),
+                text="Bubble sort, is one of the most simplest sorting algorithm that repeatedly steps through the list, compares adjacent pairs and swaps them if they are in the wrong order.",
+                format=TextFormat(fontSize=31, pos="center", warpText=48)
+            ))
+        )\
+        .addSurface(
+            Surface(
+                name="speed",
+                type_="button",
+                frame=Frame(x=271, y=480, w=704, h=70),
+                actions=[]
             )
+            .addData("image", Images(None, Frame(x=271, y=480, w=704, h=70)))
+            .addData("text", Text(
+                frame=Frame(x=296, y=490, w=654, h=53),
+                text="100.0", suffix=" swaps per sec",
+                format=TextFormat(fontSize=38, pos="center")
+            ))
+        )\
+        .addSurface(
+            Surface(
+                name="length",
+                type_="button",
+                frame=Frame(x=403, y=575, w=572, h=70),
+                actions=[]
+            )
+            .addData("image", Images(None, Frame(x=403, y=575, w=572, h=70)))
+            .addData("text", Text(
+                frame=Frame(x=428, y=585, w=522, h=53),
+                text="10", suffix=" bars",
+                format=TextFormat(fontSize=38, pos="center")
+            ))
+        )\
+        .addSurface(
+            Surface(
+                name="run",
+                type_="button",
+                frame=Frame(x=781, y=671, w=194, h=70),
+                actions=[]
+            )
+            .addData("image", Images(None, Frame(x=781, y=671, w=194, h=70)))
         )
     
     def start(self):
-        self.getSurface("info").load(withChilds=["test"], nested=True)
+        pass
